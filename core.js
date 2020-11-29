d7a409f82f8802f05d23d149cb4d4f95a9ebe00f
Henriquetied <hgoulart659@gmail.com>
2020-11-29 17:16:08 -0300

    commit

--- core.js
+++ core.js
@@ -1,11 +1,24 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
 const port = 3000
+let data = new Date()
+let hora = data.getHours()
+let minuto = data.getMinutes()
 
 app.use(express.static('public'))
+app.use(cors)
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.post('/send', (req, res, next) => {
+
+    
+
+})
 
 app.listen(port, () => {
 
-    console.log(`Servidor rodando na porta ${port}`)
+    console.log(`Servidor rodando porta ${port}, link de acesso => http://localhost:3000/. Hora atual => ${hora}:${minuto}`)
 
 })
